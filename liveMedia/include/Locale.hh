ab90e8bc1c803837414ff1180f3213f1827a0efc
KO Myung-Hun <komh78@gmail.com>
2026-07-15 16:11:05 +0900

    Disable NEWLOCALE

--- liveMedia/include/Locale.hh
+++ liveMedia/include/Locale.hh
@@ -39,7 +39,7 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 #undef LOCALE_NOT_USED
 #undef NEWLOCALE_NOT_USED
 #else
-#if defined(__WIN32__) || defined(_WIN32)
+#if defined(__WIN32__) || defined(_WIN32) || defined(__OS2__)
 #define NEWLOCALE_NOT_USED 1
 #endif
 #endif
